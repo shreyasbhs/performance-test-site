5b1d7ee080f14af86738c467014abc6e93379538
Shreyas S <shres@akamai.com>
2024-02-08 17:22:42 +0530

    enabled https

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const crypto = require('crypto');
 const nunjucks = require('nunjucks');
+const fs = require('fs');
+const https = require('https');
 const path = require('path');
 
 const app = express();
 const PORT = 8091;
+const SSL_PORT = 443;
 
 //Change the default views directory
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/dctauto.shreyasbhs.in/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/dctauto.shreyasbhs.in/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/dctauto.shreyasbhs.in/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
 
 
 nunjucks.configure('public/templates', {
@@ -18,6 +32,9 @@ nunjucks.configure('public/templates', {
 
 
 app.set('view engine', 'nunjucks');
+
+// app.use(express.static(__dirname, { dotfiles: 'allow' }));
+
 app.get('/', (req, res) => { 
     const allowedDomains = "*.cloudfront.net assets-global.website-files.com cdnjs.cloudflare.com cdnjs.cloudflare.com *.webformscr.com amosk.com.ua"
     const nonce = crypto.randomBytes(16).toString('base64');
@@ -30,6 +47,14 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.info('Server is listening on port:\t' + PORT);
-})
+});
+
+
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(SSL_PORT, () => {
+    console.log('HTTPS Server running on port:\t', SSL_PORT);
+});
